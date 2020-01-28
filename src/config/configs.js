1395569a7a480baafdbaa10cd981e97a21df5517
devleonardorabelo <leonardomrabelo@gmail.com>
2020-01-28 16:26:58 -0300

    Removed files

--- src/config/configs.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'paymentConfigs': {
-        'publicKey': 'pk_test_pPOC14ljvMHYJOODJ7mOXtJe00Pter1jsE',
-        'secretKey': 'sk_test_C7mmc6ytFxBLwtc3yE2YzzUc00uTFsZUQB',
-        'productID': '12345',
-        'domainURL': 'http://localhost:21068'
-    }
-}
