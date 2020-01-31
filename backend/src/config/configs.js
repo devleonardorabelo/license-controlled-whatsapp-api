27383c602f8f1b7955ee02c108a7133f21caac1d
devleonardorabelo <devleonardorabelo@gmail.com>
2020-01-31 17:47:34 -0300

    Added a interface React

---
+++ backend/src/config/configs.js
@@ -0,0 +1,15 @@
+module.exports = {
+	'paymentConfigs': {
+		'productId': '12345',
+		'domainUrl': 'http://localhost:21068',
+		'publickKey': '',
+		'secretKey': '',
+	},
+	'mailerConfigs': {
+		'host': 'smtp.umbler.com',
+		'port': 587,
+		'noReplyMail': 'noreply@wule.com.br',
+		'noReplyPwd': 'Leo#91427507',
+	}
+
+}
